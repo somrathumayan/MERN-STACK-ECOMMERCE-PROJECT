92c4be022c175690f1dd8d1311905bd7c5784832
somrathumayan <somrathumayan@gmail.com>
2025-02-05 00:55:47 +0600

    edited

--- server/src/server.js
+++ server/src/server.js
@@ -4,14 +4,25 @@ const morgan = require("morgan");
 const app = express();
 app.use(morgan("dev"));
 
+// middleware
+const isLoggedIn = (req, res, next) => {
+    console.log("Middleware Products found");
+    next();
+};
+
 app.get("/", (req, res) => {
     res.send("Welcome to the server");
 });
+
+app.get("/prod", isLoggedIn , (req, res) => {
+    res.send("Get: Middleware Products found")
+})
 
 app.get("/products", (req, res) => {
     res.send("Get: Products found")
 })
 
+/*
 app.put("/products", (req, res) => {
     res.send("Put: Products found")
 })
@@ -23,7 +34,10 @@ app.post("/products", (req, res) => {
 app.delete("/products", (req, res) => {
     res.send("Delete: Products found")
 })
+**/
+
 
 app.listen(3001, () => {
     console.log("Server is running at port 3001");
 })
+
